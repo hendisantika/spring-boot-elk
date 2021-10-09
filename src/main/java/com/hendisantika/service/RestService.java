9d46fa01f58f5ae95bf3e2150b59ad59e17ffd85
Hendi Santika <hendisantika@gmail.com>
2021-10-09 07:42:09 +0700

    #6 - Add getPostsPlainJSON in RestService

--- src/main/java/com/hendisantika/service/RestService.java
+++ src/main/java/com/hendisantika/service/RestService.java
@@ -1,5 +1,7 @@
 package com.hendisantika.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -24,5 +26,20 @@ public class RestService {
 
     public RestService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public JsonNode getPostsPlainJSON(String city) {
+        log.info("Getting Weather Information for {}", city);
+        String url = BASE_URL + city;
+        log.info("Final URL => {}", url);
+        try {
+            String response = this.restTemplate.getForObject(url, String.class);
+            ObjectMapper mapper = new ObjectMapper();
+            log.info("Response => {}", response);
+            return mapper.readTree(response);
+        } catch (Exception e) {
+            log.info("Error occurred in Outbound Request... => {}", e.getMessage());
+        }
+        return null;
     }
 }
