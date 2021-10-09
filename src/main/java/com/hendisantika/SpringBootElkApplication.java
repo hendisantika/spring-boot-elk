88661b34d0f8b849e03e6319634faca64c864c6f
Hendi Santika <hendisantika@gmail.com>
2021-10-09 07:32:41 +0700

    #1 - Initial commit

---
+++ src/main/java/com/hendisantika/SpringBootElkApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootElkApplication.class, args);
+    }
+
+}
